64b109d9206101e49c57e8fbba53c11fbe2bda2b
LeadNess <leadness@mail.ru>
2020-04-23 17:48:53 +0300

    Add data with train and val df
    Also update nn.py

--- nn.py
+++ nn.py
@@ -1,25 +1,20 @@
 import os
 import requests
-import shutil
 import json
-import gzip
-import shutil
 import tensorflow as tf
-import numpy as np
 import pandas as pd
-from datetime import datetime
 from sklearn.preprocessing import LabelBinarizer, MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 
 
 NUM_WORKERS = 2
-ip1 = input('Enter ip 1: ')
-ip2 = input('Enter ip 2: ')
+ip1 = '192.168.1.37'
+ip2 = '192.168.1.72'
 IP_ADDRS = [ip1, ip2]
-port1 = int(input('Enter port 1: '))
-port2 = int(input('Enter port 2: '))
+port1 = 12345
+port2 = 12345
 PORTS = [port1, port2]
-index = int(input('Enter worker index: '))
+index = 1
 
 os.environ['TF_CONFIG'] = json.dumps({
     'cluster': {
@@ -32,12 +27,12 @@ os.environ['TF_CONFIG'] = json.dumps({
     }
 })
 
-choose = input('Strategy:\n1. MultiWorkerMirroredStrategy\n2. ParameterServerStrategy')
+choose = int(input('1. MultiWorkerMirroredStrategy\n2. ParameterServerStrategy\n))
 
 if choose == 1:
-	strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 else:
-	strategy = tf.distribute.experimental.ParameterServerStrategy()
+    strategy = tf.distribute.experimental.ParameterServerStrategy()
 
 
 # Get features types dict and features list
@@ -61,60 +56,18 @@ target_classes = {
 attack_types_dict = {line.split()[0]: line.split()[1] for line in buf}
 attack_types_dict['normal'] = 'normal'
 
-#Get data
-_URL = 'http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz'
-zip_file = tf.keras.utils.get_file(origin=_URL,
-                                   fname="kddcup.data.gz",
-                                   extract=True)
+train_df = pd.read_csv(r'data\train_df.csb')
+val_df = pd.read_csv(r'data\val_df.csv')
 
-# Extract zip file with data
-data_file = 'kddcup_data'
-
-with gzip.open(zip_file, 'rb') as f_in:
-    with open(data_file, 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
-
-# Load data into df
-data = pd.read_csv(data_file, 
-                      header=None, 
-                      names=features + ['label'])
-data['label'] = [i[:-1] for i in data['label'].values]
-
-# Unnormalized numerical features
 num_attrs = []
 for i in [0, 4, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 22, 31, 32]:
     num_attrs.append(features[i])
+
 
 # Categorical features
 cat_attrs = []
 for i in [1, 2, 3]:
     cat_attrs.append(features[i])
-
-# Detect small and numerous classes
-numerous_classes = ['smurf', 'neptune', 'normal', 'satan', 'ipsweep', 'portsweep']
-small_classes = ['nmap', 'back', 'warezclient', 'teardrop']
-
-# Form training, test and validation dataframes
-train_df, test_df, val_df = data[1:2], data[2:3], data[3:4]
-
-TRAIN_NUM = 2000
-TEST_NUM = 200
-
-for cl in numerous_classes:
-    train_df = train_df.merge(data[data['label']==cl][:TRAIN_NUM], how='outer')
-    test_df = test_df.merge(data[data['label']==cl][TRAIN_NUM:TRAIN_NUM+TEST_NUM], how='outer')
-    val_df = val_df.merge(data[data['label']==cl][TRAIN_NUM+TEST_NUM:TRAIN_NUM+TEST_NUM+TEST_NUM], how='outer')
-
-for cl in small_classes:
-    TRAIN_NUM = round(len(data[data['label']==cl]) * 0.8)
-    TEST_NUM = round(len(data[data['label']==cl]) * 0.1)
-    train_df = train_df.merge(data[data['label']==cl][:TRAIN_NUM], how='outer')
-    test_df = test_df.merge(data[data['label']==cl][TRAIN_NUM:TRAIN_NUM+TEST_NUM], how='outer')
-    val_df = val_df.merge(data[data['label']==cl][TRAIN_NUM+TEST_NUM:TRAIN_NUM+TEST_NUM+TEST_NUM], how='outer')
-    
-print('train_df: ', len(train_df))
-print('test_df:  ', len(test_df)) 
-print('val_df:   ', len(val_df))
 
 
 train_data = train_df.copy()
@@ -138,11 +91,11 @@ batch_size=BATCH_SIZE
 cat_encoder = LabelBinarizer()
 scaler = MinMaxScaler()
 for attr in cat_attrs:
-    cat_encoder.fit(data[attr].values.reshape(-1, 1))
+    cat_encoder.fit(train_data[attr].values.reshape(-1, 1))
     train_df[attr] = cat_encoder.transform(train_df[attr].values.reshape(-1, 1))
     val_df[attr] = cat_encoder.transform(val_df[attr].values.reshape(-1, 1))
 for attr in num_attrs:
-    scaler.fit(data[attr].values.reshape(-1, 1))
+    scaler.fit(train_data[attr].values.reshape(-1, 1))
     train_df[attr] = scaler.fit_transform(train_df[attr].values.reshape(-1, 1))
     val_df[attr] = scaler.fit_transform(val_df[attr].values.reshape(-1, 1))
             
@@ -187,3 +140,4 @@ with strategy.scope():
         use_multiprocessing=True, 
         epochs=epochs,
     )
+
